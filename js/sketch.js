b70c1b6a6b1b091019cf22a21d34a94cd9808d72
Swastik WhitehatJR <81065922+Swastik-WhitehatJR@users.noreply.github.com>
2021-05-17 09:22:24 +0530

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,88 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Events = Matter.Events;
+const Bodies = Matter.Bodies;
+ 
+
+var particles = [];
+var plinkos = [];
+var divisions =[];
+
+
+function preload() {
+  playbg = loadImage("images/pinkobackground.jpg")
+
+  homebg = loadImage("images/homebg.jfif");
+
+  logoimg = loadImage("images/logo.jfif");
+
+}
+
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-300/2, 10, 300));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+}
+ 
+
+
+function draw() {
+  background(playbg);
+  textSize(20)
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+
+   if(frameCount % 5 === 0){
+     particles.push(new Particle(random(width/2-100, width/2+100), 10,10));
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
